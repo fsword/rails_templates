2a19b49eddbdfa57ee0e82eb8c7fc6132133dd97
fsword <li.jianye@gmail.com>
2011-05-04 14:56:57 +0800

    添加简化版template

---
+++ jruby_template0.rb
@@ -0,0 +1,30 @@
+require "thor/shell"
+require 'openssl'
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
+say "setting up the Gemfile...", :yellow
+
+run 'cp config/database.yml config/default.database.yml'
+gem 'activerecord-jdbcsqlite3-adapter'
+#更改jdbc驱动（gem命令只能添加gem，但是不能去掉缺省的gem）
+gsub_file 'Gemfile', /gem\ 'sqlite3'/, "gem 'jdbc-sqlite3'"  
+gsub_file 'config/database.yml', /adapter:\ +sqlite3/, "adapter: jdbcsqlite3" 
+
+# Install gems
+say("installing gems (takes a few minutes!)...", :yellow)
+run 'bundle install'
+
+application do
+  "
+    require 'openssl'
+    OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+    config.time_zone = 'Beijing'
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :'zh-CN'
+    config.after_initialize do
+      config.active_record.default_timezone = :local
+    end
+  "
+end
+
+say("Done setting up your Rails app.", :yellow)
